dbd89b4f37db3f18336d4249a154b93408ad35dc
Bennet Gallein <me@bennetgallein.de>
2025-08-30 11:40:17 +0200

    feat: switch auth endpoint to post

--- build/plugin/rest_auth/rest_auth.go
+++ build/plugin/rest_auth/rest_auth.go
@@ -2,29 +2,30 @@ package main
 
 import (
 	"bytes"
+	"crypto/tls"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"crypto/tls"
-	"fmt"
+
 	"github.com/tg123/sshpiper/libplugin"
 	"golang.org/x/crypto/ssh"
 )
 
 type plugin struct {
-	URL        string
+	URL      string
 	Insecure bool
 }
 
 type piperTo struct {
-	User string
-	Host string
+	User           string
+	Host           string
 	AuthorizedKeys string
-	PrivateKey string
+	PrivateKey     string
 }
 
-func newRestAuthPlugin() *plugin{
+func newRestAuthPlugin() *plugin {
 	return &plugin{}
 }
 
@@ -44,12 +45,18 @@ func (p *plugin) supportedMethods() ([]string, error) {
 func (p *plugin) findAndCreateUpstream(conn libplugin.ConnMetadata, password string, publicKey []byte) (*libplugin.Upstream, error) {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: p.Insecure}
 	user := conn.User()
-	resp, err := http.Get(p.URL + fmt.Sprintf("/%s", url.QueryEscape(user)))
+	bodyData := map[string]string{
+		"user":       user,
+		"remoteAddr": conn.RemoteAddr(),
+	}
+	jsonBody, _ := json.Marshal(bodyData)
+	resp, err := http.Post(p.URL+fmt.Sprintf("/%s", url.QueryEscape(user)), "application/json", bytes.NewBuffer(jsonBody))
+
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-	if resp.StatusCode == 200 {
+	if resp.StatusCode == 201 {
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			return nil, err
@@ -58,10 +65,10 @@ func (p *plugin) findAndCreateUpstream(conn libplugin.ConnMetadata, password str
 		json.Unmarshal([]byte(body), &result)
 
 		to := piperTo{
-			User:fmt.Sprint(result["user"]),
-			Host:fmt.Sprint(result["host"]),
-			AuthorizedKeys:fmt.Sprint(result["authorizedKeys"]),
-			PrivateKey:fmt.Sprint(result["privateKey"]),
+			User:           fmt.Sprint(result["user"]),
+			Host:           fmt.Sprint(result["host"]),
+			AuthorizedKeys: fmt.Sprint(result["authorizedKeys"]),
+			PrivateKey:     fmt.Sprint(result["privateKey"]),
 		}
 		rest, err := p.strToByte(to.AuthorizedKeys, map[string]string{
 			"DOWNSTREAM_USER": user,
@@ -79,8 +86,8 @@ func (p *plugin) findAndCreateUpstream(conn libplugin.ConnMetadata, password str
 				return p.createUpstream(conn, to)
 			}
 		}
-	}else {
-		return nil, err
+	} else {
+		return nil, fmt.Errorf("no matching pipe for username [%v] found", user)
 	}
 	return nil, fmt.Errorf("no matching pipe for username [%v] found", user)
 }
